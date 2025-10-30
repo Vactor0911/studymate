306902188fdf240dfc515806b59bd31d8c5b06f6
Vactor0911 <vactor0911@gmail.com>
2025-10-31 05:38:56 +0900

    커리큘럼 목록 페이지 구현

--- frontend/src/pages/Curriculum/index.tsx
+++ frontend/src/pages/Curriculum/index.tsx
@@ -1,282 +1,154 @@
 import {
   Box,
-  Divider,
-  IconButton,
+  Button,
+  Container,
+  MenuItem,
+  Select,
   Stack,
-  Tab,
-  Tabs,
   Typography,
-  useMediaQuery,
-  useTheme,
 } from "@mui/material";
-import {
-  Panel,
-  PanelGroup,
-  type ImperativePanelHandle,
-} from "react-resizable-panels";
-import TitledContainer from "./TitledContainer";
-import StyledPanelResizeHandle from "./StyledPanelResizeHandle";
-import CurriculumViewer from "./CurriculumViewer";
-import { useCallback, useRef, useState } from "react";
-import { useAtom } from "jotai";
-import { roadmapTabAtom } from "../../states";
-import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
-import CurriculumChatBot from "./CurriculumChatBot";
-import RoadMapDetails, { type NodeDetail } from "./RoadMapDetails";
+import { useCallback, useState } from "react";
+import CurriculumItem from "./CurriculumItem";
+
+type CurriculumType = {
+  createdAt: string;
+  subject: string;
+  grade: string;
+  detail: string;
+};
+
+const CurriculumData = [
+  {
+    createdAt: "2025 / 10 / 30",
+    subject: "수학",
+    grade: "중학교 3학년",
+    detail: "기본 연산과 방정식을 학습합니다",
+  },
+  {
+    createdAt: "2025 / 10 / 30",
+    subject: "수학",
+    grade: "중학교 3학년",
+    detail: "기본 연산과 방정식을 학습합니다",
+  },
+  {
+    createdAt: "2025 / 10 / 30",
+    subject: "수학",
+    grade: "중학교 3학년",
+    detail: "기본 연산과 방정식을 학습합니다",
+  },
+] as CurriculumType[];
 
 const Curriculum = () => {
-  const theme = useTheme();
-  const isPC = useMediaQuery(theme.breakpoints.up("lg"));
+  const [grade, setGrade] = useState(1);
+  const [subject, setSubject] = useState("과목");
+  const [duration, setDuration] = useState("기간");
+  const [curriculums, ] = useState(CurriculumData);
 
-  // 화면 크기
-  const isMd = useMediaQuery(theme.breakpoints.only("md"));
-  const isLg = useMediaQuery(theme.breakpoints.only("lg"));
-  const isXl = useMediaQuery(theme.breakpoints.only("xl"));
-
-  const [isDetailsOpen, setIsDetailsOpen] = useState(true); // 상세정보 패널 열림 상태
-  const [isChatbotOpen, setIsChatbotOpen] = useState(true); // 챗봇 패널 열림 상태
-  const detailsPanel = useRef<ImperativePanelHandle>(null); // 상세정보 패널 참조
-  const chatbotPanel = useRef<ImperativePanelHandle>(null); // 챗봇 패널 참조ㄴ
-
-  // 상세정보 상태/로딩 관리
-  const [selectedNodeDetail, setSelectedNodeDetail] =
-    useState<NodeDetail | null>(null);
-  const [detailLoading, setDetailLoading] = useState(false);
-
-  // 탭 메뉴
-  const [tab, setTab] = useAtom(roadmapTabAtom);
-
-  // 상세정보 패널 열기/닫기
-  const handleDetailsToggle = useCallback(() => {
-    const newOpenState = !isDetailsOpen;
-    setIsDetailsOpen(newOpenState);
-
-    if (detailsPanel.current) {
-      if (newOpenState) {
-        detailsPanel.current.expand();
-      } else {
-        detailsPanel.current.collapse();
-      }
-    }
-  }, [isDetailsOpen]);
-
-  // 상세정보 패널 열림 상태 적용
-  const handleSetDetailsOpen = useCallback((open: boolean) => {
-    setIsDetailsOpen(open);
-
-    if (detailsPanel.current) {
-      if (open) {
-        detailsPanel.current.expand();
-      } else {
-        detailsPanel.current.collapse();
-      }
+  const getColor = useCallback((index: number) => {
+    switch (index) {
+      case 0:
+        return "#EFF7F6";
+      case 1:
+        return "#D8EEEB";
+      default:
+        return "#BCE6E0";
     }
   }, []);
 
-  // 챗봇 패널 열기/닫기
-  const handleChatbotToggle = useCallback(() => {
-    const newOpenState = !isChatbotOpen;
-    setIsChatbotOpen(newOpenState);
-
-    if (chatbotPanel.current) {
-      if (newOpenState) {
-        chatbotPanel.current.expand();
-      } else {
-        chatbotPanel.current.collapse();
-      }
-    }
-  }, [isChatbotOpen]);
-
-  // 챗봇 패널 열림 상태 적용
-  const handleSetChatbotOpen = useCallback((open: boolean) => {
-    setIsChatbotOpen(open);
-
-    if (chatbotPanel.current) {
-      if (open) {
-        chatbotPanel.current.expand();
-      } else {
-        chatbotPanel.current.collapse();
-      }
-    }
-  }, []);
-
-  // 탭 메뉴 변경
-  const handleTabChange = useCallback(
-    (_: React.SyntheticEvent, newValue: number) => {
-      setTab(newValue);
-    },
-    [setTab]
-  );
-
-  // 패널 축소 크기 반환
-  const getCollapsedSize = useCallback(() => {
-    if (isMd) return 5; // 태블릿
-    if (isLg) return 4; // 작은 PC
-    if (isXl) return 3; // 큰 PC
-  }, [isLg, isMd, isXl]);
-
-  // PC 화면 렌더링
-  if (isPC) {
-    return (
-      <Stack padding={2} direction="row" height="calc(100vh - 64px)">
-        <PanelGroup direction="horizontal">
-          {/* 상세정보 패널 */}
-          <Panel
-            ref={detailsPanel}
-            defaultSize={25}
-            minSize={20}
-            collapsible
-            collapsedSize={getCollapsedSize()}
-            onCollapse={() => handleSetDetailsOpen(false)}
-            onExpand={() => handleSetDetailsOpen(true)}
-            css={{
-              height: "100%",
-            }}
-          >
-            <TitledContainer
-              title="상세정보"
-              collapsed={!isDetailsOpen}
-              toggleButton={
-                <IconButton size="small" onClick={handleDetailsToggle}>
-                  <KeyboardArrowRightRoundedIcon
-                    sx={{
-                      transform: isDetailsOpen
-                        ? "rotate(-180deg)"
-                        : "rotate(0deg)",
-                      transition: "transform 0.2s ease",
-                    }}
-                  />
-                </IconButton>
-              }
-            >
-              {detailLoading || selectedNodeDetail ? (
-                <RoadMapDetails
-                  nodeDetail={selectedNodeDetail}
-                  loading={detailLoading}
-                />
-              ) : (
-                <Typography variant="subtitle1" color="text.secondary" p={2}>
-                  노드를 클릭하면 상세정보가 표시됩니다.
-                </Typography>
-              )}
-            </TitledContainer>
-          </Panel>
-
-          {/* 구분선 */}
-          <StyledPanelResizeHandle />
-
-          {/* 로드맵 패널 */}
-          <Panel
-            minSize={20}
-            css={{
-              height: "100%",
-            }}
-          >
-            <TitledContainer title="로드맵">
-              <CurriculumViewer
-                onNodeDetail={(detail, loading) => {
-                  handleSetDetailsOpen(true); // 상세정보 패널 자동 열기
-                  setTimeout(() => {
-                    // 1.5초 후에 상세정보 탭으로 전환
-                    setTab(0);
-                  }, 1000);
-                  setSelectedNodeDetail(detail);
-                  setDetailLoading(loading);
-                }}
-              />
-            </TitledContainer>
-          </Panel>
-
-          {/* 구분선 */}
-          <StyledPanelResizeHandle />
-
-          {/* 챗봇 패널 */}
-          <Panel
-            ref={chatbotPanel}
-            defaultSize={25}
-            minSize={20}
-            collapsible
-            collapsedSize={getCollapsedSize()}
-            onCollapse={() => handleSetChatbotOpen(false)}
-            onExpand={() => handleSetChatbotOpen(true)}
-            css={{
-              height: "100%",
-            }}
-          >
-            <TitledContainer
-              title="챗봇"
-              collapsed={!isChatbotOpen}
-              toggleButton={
-                <IconButton size="small" onClick={handleChatbotToggle}>
-                  <KeyboardArrowRightRoundedIcon
-                    sx={{
-                      transform: isChatbotOpen
-                        ? "rotate(0deg)"
-                        : "rotate(-180deg)",
-                      transition: "transform 0.2s ease",
-                    }}
-                  />
-                </IconButton>
-              }
-            >
-              <CurriculumChatBot />
-            </TitledContainer>
-          </Panel>
-        </PanelGroup>
-      </Stack>
-    );
-  }
-
-  // 모바일 및 태블릿 화면 렌더링
   return (
-    <Stack paddingTop={2} height="calc(100vh - 64px)">
-      <Box>
-        <Tabs value={tab} onChange={handleTabChange} centered>
-          <Tab label="상세정보" />
-          <Tab label="로드맵" />
-          <Tab label="챗봇" />
-        </Tabs>
-        <Divider />
-      </Box>
+    <Container maxWidth="xl">
+      <Stack py={15} gap={2}>
+        {/* 필터 컨테이너 */}
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={2}
+        >
+          {/* 학년 필터 */}
+          <Stack
+            direction="row"
+            alignItems="center"
+            gap={1}
+            p={1.5}
+            bgcolor="#F6F6F6"
+            borderRadius="50px"
+          >
+            {Array.from({ length: 3 }).map((_, index) => (
+              <Button
+                key={`grade-select-${index}`}
+                variant="contained"
+                disableElevation={index + 1 !== grade}
+                color="transparent"
+                sx={{
+                  bgcolor: index + 1 === grade ? "white" : "transparent",
+                  py: 1,
+                }}
+                onClick={() => setGrade(index + 1)}
+              >
+                <Typography variant="body1" fontWeight={500}>
+                  {index + 1}학년
+                </Typography>
+              </Button>
+            ))}
+          </Stack>
 
-      {/* 상세정보 */}
-      {tab === 0 && (
-        <>
-          {detailLoading || selectedNodeDetail ? (
-            <RoadMapDetails
-              nodeDetail={selectedNodeDetail}
-              loading={detailLoading}
+          {/* 과목 및 기간 필터 */}
+          <Stack direction="row" alignItems="center" gap={2}>
+            {/* 과목 */}
+            <Box width="180px">
+              <Select
+                fullWidth
+                value={subject}
+                onChange={(event) => setSubject(event.target.value as string)}
+              >
+                <MenuItem value="과목">과목</MenuItem>
+                <MenuItem value="국어">국어</MenuItem>
+                <MenuItem value="영어">영어</MenuItem>
+                <MenuItem value="수학">수학</MenuItem>
+              </Select>
+            </Box>
+
+            {/* 기간 */}
+            <Box width="180px">
+              <Select
+                fullWidth
+                value={duration}
+                onChange={(event) => setDuration(event.target.value as string)}
+              >
+                <MenuItem value="기간">기간</MenuItem>
+                <MenuItem value="2주일">2주일</MenuItem>
+                <MenuItem value="1개월">1개월</MenuItem>
+                <MenuItem value="2개월">2개월</MenuItem>
+              </Select>
+            </Box>
+          </Stack>
+        </Stack>
+
+        {/* 성취도 평가 목록 */}
+        <Stack gap={2}>
+          {curriculums.map((item, index) => (
+            <CurriculumItem
+              key={`curriculum-item-${index}`}
+              createdAt={item.createdAt}
+              grade={item.grade}
+              subject={item.subject}
+              detail={item.detail}
+              color={getColor(index)}
             />
-          ) : (
-            <Typography variant="subtitle1" color="text.secondary" p={2}>
-              노드를 클릭하면 상세정보가 표시됩니다.
+          ))}
+        </Stack>
+
+        {/* 커리큘럼 생성 버튼 */}
+        <Box alignSelf="flex-end">
+          <Button variant="contained" color="primary">
+            <Typography variant="body1" fontWeight={500}>
+              커리큘럼 생성하기
             </Typography>
-          )}
-        </>
-      )}
-
-      {/* 로드맵 */}
-      {tab === 1 && (
-        <CurriculumViewer
-          onNodeDetail={(detail, loading) => {
-            handleSetDetailsOpen(true); // 상세정보 패널 자동 열기
-            setTimeout(() => {
-              // 1.5초 후에 상세정보 탭으로 전환
-              setTab(0);
-            }, 1000);
-            setSelectedNodeDetail(detail);
-            setDetailLoading(loading);
-          }}
-        />
-      )}
-
-      {/* 챗봇 */}
-      {tab === 2 && (
-        <Box padding={2} height="calc(100% - 50px)">
-          <CurriculumChatBot />
+          </Button>
         </Box>
-      )}
-    </Stack>
+      </Stack>
+    </Container>
   );
 };
 
