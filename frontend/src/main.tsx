a7fc33478328363a3b2d9fa461dd434e2570efdf
Vactor0911 <vactor0911@gmail.com>
2025-10-30 15:23:39 +0900

    로그인 및 회원가입 페이지 구현

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,16 @@ import { Provider as JotaiProvider } from "jotai";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./utils/theme.ts";
 import { jotaiStore } from "./states/jotaiStore.ts";
+import CustomSnackbarProvider from "./components/CustomSnackbarProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <JotaiProvider store={jotaiStore}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
+        <CustomSnackbarProvider>
+          <CssBaseline />
+          <App />
+        </CustomSnackbarProvider>
       </ThemeProvider>
     </JotaiProvider>
   </StrictMode>
